548f379736989451f67c4f40e3cb0f7018a38989
Costin Zaharia <cgzaharia@gmail.com>
2016-08-23 19:54:42 +0300

    Extract common AssemblyInfo

---
+++ src/VSGerrit/Properties/CommonInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("https://github.com/vsgerrit/vsgerrit")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
